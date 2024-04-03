1244d4ddbce40ee8a44e4e9dfacd5e2ed441712d
Giulio Surya <giuliosurya.loverde@studio.unibo.it>
2024-04-03 14:42:51 +0200

    change

--- code.r
+++ code.r
@@ -1,11 +1,1 @@
-#inizial commit test
-#aksndoasndoiasdoiasndias
-nnvdjvnjnjevnjevwevcbbb hhhh ggg
-
-#ok top sono riuscito a modificare da vscode e si sincronizza su github in automatico
-ffff
-#si
-
-#incolla qui
-dkdkdkdkdkdk fffff
-
+library(googlesheets4)
