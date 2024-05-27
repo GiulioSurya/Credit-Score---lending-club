a9ab43778f7c40d033950a4f798c204630c708db
Giulio Surya <giuliosurya.loverde@studio.unibo.it>
2024-05-27 15:57:29 +0200

    update arfima+garch

--- TimeSeries.r
+++ TimeSeries.r
@@ -10,6 +10,7 @@ remove(list = ls())
   library(rugarch)
   library(MASS)
   library(fGarch)
+  library(fracdiff)
 }
 
 
@@ -19,7 +20,7 @@ remove(list = ls())
 ts_raw<-read_excel("FEDFUNDS.xlsx")
 
 ts_raw$observation_date <- substr(ts_raw$observation_date, 1, 7) #remove the day
-nchar(ts$observation_date)[nchar(ts$observation_date) != 7] #all the data has 7 characters: aaaa-mm
+nchar(ts_raw$observation_date)[nchar(ts$observation_date) != 7] #all the data has 7 characters: aaaa-mm
 
 #creating a time series object
 ts <- ts(ts_raw$FEDFUNDS, start = c(as.yearmon("1954-07")), end = c(as.yearmon("2024-02")), frequency = 12)
@@ -52,6 +53,7 @@ plot(ts[test], type="l")
 lines(pred, col="red")
 acf(residuals(fit.is))
 checkresiduals(fit.is)
+
 
 
 rmse <- sqrt(mean((ts[test] - mean(pred))^2))
@@ -89,10 +91,11 @@ ts_diff <- diff(ts)
 ARIMA <- Arima(ts_diff, order = c(1, 0, 1))
 acf(residuals(ARIMA)^2) #still some correlation, we wanna try to model the variance
 
-
+order<-fracdiff(ts, nar = 1, nma = 1)
 spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
-                   mean.model = list(armaOrder = c(1, 1), include.mean = F), distribution.model = "std")
-fit <- ugarchfit(spec, data = ts_diff[train])
+                   mean.model = list(armaOrder = c(1, 1), include.mean = F,arfima=T), 
+                  fixed.pars=list(arfima =order$d ), distribution.model = "std")
+fit <- ugarchfit(spec, data = ts[train])
 
 
 ut<-fit@fit$residuals/fit@fit$sigma #this are the standardized residuals, , in this series the mean is=0
@@ -111,38 +114,38 @@ pred <- rep(NA, n)
 up_int<-rep(NA, n)
 low_int<-rep(NA, n)
 
+
+spec <- ugarchspec(variance.model = list(model = "sGARCH", garchOrder = c(1, 1)),
+                   mean.model = list(armaOrder = c(1,1), include.mean = F, arfima=T), 
+                  fixed.pars=list(arfima =order$d ), distribution.model = "std")
+fit <- ugarchfit(spec, data=ts[train])
+
 for (i in 1:n) {
   train_up <- c(train, test[1:i])
   fit_coef<-getspec(fit)
-  setfixed(fit_coef)<-as.list(coef(fit))
+  setfixed(fit_coef)<-as.list(coef(fit));
   forecast_up <- ugarchforecast(fit_coef, data=ts[train_up],n.ahead = h)
   pred[i] <- forecast_up@forecast$seriesFor[h]
-  sigma <- forecast_up@forecast$sigmaFor
-  #de-diff
-  pred[i] <- ts[train_up[length(train_up)]] + pred[i]
-  up_int[i] <- pred[i] + qstd(0.975, mean=0, sd=1,nu=3) * sigma
-  low_int[i] <- pred[i] - qstd(0.975, mean=0, sd=1,nu=3) * sigma
+  sigma<- forecast_up@forecast$sigmaFor[h]
+  up_int[i] <- pred[i] + qstd(0.975, mean=0, sd=1,nu=4) * sigma
+  low_int[i] <- pred[i] - qstd(0.975, mean=0, sd=1,nu=4) * sigma
 }
 plot(ts[test], type = "l")
 lines(pred, col = "red")
 lines(up_int, col = "blue", lty = 2)
 lines(low_int, col = "blue", lty = 2)
 
-rmse <- sqrt(mean((ts[test] - mean(pred))^2))
+
+rmse <- sqrt(mean((ts[test] - pred)^2))
 rmse
 
 #fit final model
-fit_final <- ugarchfit(spec, data = ts_diff)
-
-#plot conditional variance
-df_diff_fit <- data.frame(time = time(ts_diff), fitted = fitted(fit_final))
-plot(abs(df_diff_fit$fitted), type = "l")
-lines(fit_final@fit$sigma, col = 2)
+fit_final <- ugarchfit(spec, data = ts)
 
 #plot autocorrelation of (standardized) squared residuals to compare with N(0,1)
 acf(abs(fit_final@fit$residuals / fit_final@fit$sigma))
 plot(density(fit_final@fit$residuals / fit_final@fit$sigma))
-lines(seq(-6,6,by=0.01),dnorm(seq(-6,6,by=0.01)),col=2)
+lines(seq(-6,6,by=0.01),dstd(seq(-6,6,by=0.01),0,1,4),col=2)
 
 #one year forecast
 forecast_final <- ugarchforecast(fit_final, n.ahead = 12)
@@ -151,35 +154,17 @@ forecast_se <- forecast_final@forecast$sigmaFor
 
 #confidence interval 95%
 alpha <- 0.05
-z_score <- qstd(1 - alpha / 2, mean = 0, sd = 1,nu=3) 
+z_score <- qstd(1 - alpha / 2, mean = 0, sd = 1,nu=4) 
 forecast_values_upper <- forecast_values + z_score * forecast_se
 forecast_values_lower <- forecast_values - z_score * forecast_se
 
-# De-diff
-last_value <- ts[length(ts)]
-forecast_values_de_diff <- cumsum(forecast_values) + last_value
-forecast_values_upper_de_diff <- cumsum(forecast_values_upper) + last_value
-forecast_values_lower_de_diff <- cumsum(forecast_values_lower) + last_value
+#plot last 30 values of the time series and the forecast
+plot_length <- 30
+plot_start <- length(ts) - plot_length + 1
+plot_end <- length(ts)+20
 
-#forecast plot
-extended_ts <- c(ts, forecast_values_de_diff)
-
-
-plot_length <- 100 + 12
-plot_start <- length(extended_ts) - plot_length + 1
-plot_end <- length(extended_ts)
-
-
-plot(extended_ts[plot_start:plot_end], type = "l", col = "blue", ylab = "Value", xlab = "Time", ylim = c(0, 10))
-lines(c(rep(NA, 100), forecast_values_de_diff), col = "red")
-lines(c(rep(NA, 100), forecast_values_upper_de_diff), col = "green", lty = 2)
-lines(c(rep(NA, 100), forecast_values_lower_de_diff), col = "green", lty = 2)
+plot(ts[plot_start:plot_end], type = "l", col = "blue", ylab = "Value", xlab = "Time", ylim = c(0, 10))
+lines(c(rep(NA, 30), forecast_values), col = "red")
++lines(c(rep(NA, 30), forecast_values_upper), col = "green", lty = 2)
+lines(c(rep(NA, 30), forecast_values_lower), col = "green", lty = 2)
 legend("topleft", legend = c("Time Series", "Forecast", "Confidence Interval"), col = c("blue", "red", "green"), lty = c(1, 1, 2))
-
-
-
-
-
-
-
-
